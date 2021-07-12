bbb6b7d63da6f7a4231416c61ec382e7988247af
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2021-07-12 16:40:39 +0200

    Implemented Order Based LP

--- linprog.py
+++ linprog.py
@@ -2,6 +2,7 @@ from abc import ABC
 from abc import abstractmethod
 import docplex.mp.model
 import numpy as np
+import os
 
 
 class LP(ABC):
@@ -36,8 +37,10 @@ class LP(ABC):
 
     def solve(self):
         """Solve the LP."""
+        # self._problem.export_as_lp(os.getcwd())
         if self._solver == 'cplex':
-            self._problem.solve()
+            self._problem.solve(log_output=True)
+            self._problem.print_solution()
         else:
             raise NotImplementedError
 
@@ -67,6 +70,9 @@ class OrderBasedSubProblem(LP):
             - 4: deadline (d_j);
             - 5: weight (W_j);
             - 6: objective constant (B_j).
+    resource : float
+        Amount of resource available per time unit. Required to be
+        constant for now.
     label : str
         Label or name for the (solved) instance. Be sure to use one that
         is sufficiently unique, to avoid conflicts or other unexpected
@@ -75,12 +81,15 @@ class OrderBasedSubProblem(LP):
         The solver that will be used to solve the LP constructed from the
         provided instance. Currently only CPLEX is supported.
     """
-    def __init__(self, eventlist, jobs, label, solver='cplex'):
+    def __init__(self, eventlist, jobs, resource, label, solver='cplex'):
         super().__init__(label, solver)
 
         self._event_list = eventlist
-        self._event_map = _construct_event_mapping(eventlist, jobs.shape[0])
+        self._event_map = self._construct_event_mapping(eventlist,
+                                                        jobs.shape[0])
         self._job_properties = jobs
+        self._capacity = resource
+        self.initialize_problem()
 
     @property
     def event_list(self):
@@ -93,6 +102,10 @@ class OrderBasedSubProblem(LP):
     @property
     def job_properties(self):
         return self._job_properties
+
+    @property
+    def capacity(self):
+        return self._capacity
 
     @staticmethod
     def _construct_event_mapping(eventlist, no_jobs):
@@ -117,6 +130,115 @@ class OrderBasedSubProblem(LP):
         # Not necessary if all jobs have exactly one start and completion
         # time.
         # event_map.fill(-1)
-        for i in eventlist.shape[0]:
-            eventmap[eventlist[i, 1], eventlist[i, 0]] = i
+        for i in range(eventlist.shape[0]):
+            event_map[eventlist[i, 1], eventlist[i, 0]] = i
         return event_map
+
+    def initialize_problem(self):
+        """...
+        """
+
+        # Create variables
+        times = np.zeros(shape=len(self._event_list), dtype=object)
+        resource = np.zeros(
+            shape=(len(self._job_properties), len(self._event_list)),
+            dtype=object
+        )
+
+        for e in range(len(self._event_list)):
+            times[e] = self._problem.continuous_var(
+                name=f"t_{e}",
+                lb=0
+            )
+
+        for j in range(len(self._job_properties)):
+            for e in range(self._event_map[j, 0], self._event_map[j, 1]):
+                resource[j, e] = self._problem.continuous_var(
+                    name=f"p_{j},{e}",
+                    lb=0
+                )
+
+        # Define objective
+        self._cost = self._problem.sum(
+            self._job_properties[j, 5] * times[self._event_map[j, 1]] +
+            self._job_properties[j, 6]
+            for j in range(len(self._job_properties))
+        )
+        self._obj = self._problem.maximize(self._cost)
+
+        # Initialize constraints
+        self._c_order = np.zeros(shape=len(self._event_list) - 1,
+                                 dtype=object)
+        self._c_availability = np.zeros(shape=len(self._event_list) - 1,
+                                        dtype=object)
+        self._c_deadline = np.zeros(shape=len(self._job_properties),
+                                    dtype=object)
+        self._c_release = np.zeros(shape=len(self._job_properties),
+                                   dtype=object)
+        self._c_resource = np.zeros(shape=len(self._job_properties),
+                                    dtype=object)
+        self._c_lower = np.zeros(shape=(len(self._job_properties),
+                                        len(self._event_list) - 1),
+                                 dtype=object)
+        self._c_upper = np.zeros(shape=(len(self._job_properties),
+                                        len(self._event_list) - 1),
+                                 dtype=object)
+
+        for e in range(len(self._event_list) - 1):
+            # 1. Event Order
+            self._c_order[e] = self._problem.add_constraint(
+                ct=times[e] - times[e + 1] <= 0,
+                ctname=f"event_order_{e}"
+            )
+            # 7. Resource availability
+            self._c_availability = self._problem.add_constraint(
+                ct=self._problem.sum(
+                    resource[j, e] for j in range(
+                        len(self._job_properties)
+                    )
+                ) - self._capacity * (times[e + 1] - times[e]) <= 0,
+                ctname=f"resource_availability_{e}"
+            )
+
+        for j in range(len(self._job_properties)):
+            # 2. Deadline
+            self._c_deadline[j] = self._problem.add_constraint(
+                ct=times[self._event_map[j, 1]] - self._job_properties[j, 4]
+                <= 0,
+                ctname=f"deadline_{j}"
+            )
+            # 3. Release date
+            self._c_release[j] = self._problem.add_constraint(
+                ct=times[self._event_map[j, 1]] - self._job_properties[j, 3]
+                >= 0,
+                ctname=f"release_{j}"
+            )
+            # 4. Resource requirement
+            self._c_resource[j] = self._problem.add_constraint(
+                ct=self._problem.sum(
+                    resource[j, e] for e in range(
+                        self._event_map[j, 0], self._event_map[j, 1]
+                    )
+                ) - self._job_properties[j, 0] == 0,
+                ctname=f"resource_{j}"
+            )
+
+            for e in range(self._event_map[j, 0], self._event_map[j, 1]):
+                # 5. Lower bound
+                self._c_lower[j, e] = self._problem.add_constraint(
+                    ct=resource[j, e] -
+                    self._job_properties[j, 1] * (times[e + 1] - times[e])
+                    >= 0,
+                    ctname=f"lower_bound_{j},{e}"
+                )
+                # 6. Upper bound
+                self._c_upper[j, e] = self._problem.add_constraint(
+                    ct=resource[j, e] -
+                    self._job_properties[j, 2] * (times[e + 1] - times[e])
+                    <= 0,
+                    ctname=f"upper_bound_{j},{e}"
+                )
+
+    def print_solution(self):
+        """Print a human readable version of the (current) solution."""
+        pass
---
+++ test_linprog.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from linprog import OrderBasedSubProblem
+
+def test_small_instance():
+    # General Eventlist
+    # eventlist = np.array([[e, i] for i in range(3) for e in range(2)])
+    # Feasible eventlist
+    eventlist = np.array([
+        [0, 2], [0, 1], [1, 2], [1, 1], [0, 0], [1, 0]
+    ])
+    jobs = np.array([
+        # E_j, P^-_j, P^+_j, r_j, d_j, W_j, B_j
+        [3, 1, 3, 1, 3, 1, 0],
+        [4, 1, 3, 0, 3, 1, 0],
+        [3, 1.5, 3, 0, 2, 1, 0]
+    ])
+    LP = OrderBasedSubProblem(eventlist, jobs, 4, 'test_problem', 'cplex')
+    LP.solve()
+
+if __name__ == "__main__":
+    test_small_instance()
