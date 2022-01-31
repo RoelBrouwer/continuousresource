231c2e785b63f3ea9d2a929724aedeac544087a0
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2022-01-31 14:47:45 +0100

    Adapt instance generation to use parameters and report flow-feasibility

--- continuousresource/scripts/multiplejobarrayinstancegeneration.py
+++ continuousresource/scripts/multiplejobarrayinstancegeneration.py
@@ -5,6 +5,8 @@ import os.path
 
 from continuousresource.probleminstances.jobarrayinstance \
     import JobPropertiesInstance
+from continuousresource.mathematicalprogramming.linprog \
+    import FeasibilityWithoutLowerbound
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
@@ -40,19 +42,48 @@ from continuousresource.probleminstances.jobarrayinstance \
     help="Label or name for the generated instance."
 )
 def main(exportpath, exportformat, label):
-    # TODO: think of useful parameters
-    for n in [5, 10]:  # [10, 20, 50, 100, 200]:
-        for r in [25.0, 50.0, 100.0, 200.0]:
-            for i in range(5):
-                instance = JobPropertiesInstance.generate_instance(n, r, adversarial=True)
+    params = {
+        'resource_fraction': 0.25,
+        'requirement_fraction': 0.25,
+        'release_date_shift': 0.125,
+        'processing_window_size': 2
+    }
+    with open(os.path.join(exportpath,
+                           f"{label}_instances_overview.csv"), "w") as csv:
+        # Header
+        csv.write(
+            'n;r;adversarial;serialID;feasible\n'
+        )
+        for n in [5]:
+            for r in [25.0, 50.0, 100.0, 200.0]:
+                for a in [True, False]:
+                    for i in range(5):
+                        instance = JobPropertiesInstance.generate_instance(
+                            n, r, adversarial=a, params=params
+                        )
 
-                inst_label = f"{label}_n{n}r{r:.2f}i{i}"
-                if exportformat in ['both', 'binary']:
-                    path = os.path.join(exportpath, inst_label)
-                    JobPropertiesInstance.to_binary(path, instance)
-                if exportformat in ['both', 'csv']:
-                    path = os.path.join(exportpath, inst_label)
-                    JobPropertiesInstance.to_csv(path, instance)
+                        # Solve flow problem
+                        lp = FeasibilityWithoutLowerbound(
+                            instance['jobs'],
+                            instance['constants']['resource_availability'],
+                            f'{n}-{r}-{a}-{i}'
+                        )
+                        lp.initialize_problem()
+                        feasible = lp.solve() is not None
+
+                        csv.write(
+                            f'{n};{r:.2f};{"1" if a else "0"};{i};'
+                            f'{"1" if feasible else "0"}\n'
+                        )
+
+                        inst_label = (f"{label}_n{n}r{r:.2f}"
+                                      f"a{'1' if a else '0'}i{i}")
+                        if exportformat in ['both', 'binary']:
+                            path = os.path.join(exportpath, inst_label)
+                            JobPropertiesInstance.to_binary(path, instance)
+                        if exportformat in ['both', 'csv']:
+                            path = os.path.join(exportpath, inst_label)
+                            JobPropertiesInstance.to_csv(path, instance)
 
 
 if __name__ == "__main__":
