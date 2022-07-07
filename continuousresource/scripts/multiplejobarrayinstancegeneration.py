8dea58e89aeb65a8edbf735d90242e0ecd931449
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2022-07-07 14:00:15 +0200

    Exact script used for instance generation

--- continuousresource/scripts/multiplejobarrayinstancegeneration.py
+++ continuousresource/scripts/multiplejobarrayinstancegeneration.py
@@ -43,10 +43,54 @@ from continuousresource.mathematicalprogramming.linprog \
 )
 def main(exportpath, exportformat, label):
     params = {
-        'resource_fraction': 0.25,
-        'requirement_fraction': 0.25,
-        'release_date_shift': 0.125,
-        'processing_window_size': 2
+        '5': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 2
+        },
+        '10': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 2
+        },
+        '15': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 1.5
+        },
+        '20': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 1.5
+        },
+        '50': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 1.5
+        },
+        '100': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 1
+        },
+        '200': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 1
+        },
+        '300': {
+            'resource_fraction': 0.25,
+            'requirement_fraction': 0.25,
+            'release_date_shift': 0.125,
+            'processing_window_size': 0.75
+        }
     }
     with open(os.path.join(exportpath,
                            f"{label}_instances_overview.csv"), "w") as csv:
@@ -54,12 +98,12 @@ def main(exportpath, exportformat, label):
         csv.write(
             'n;r;adversarial;serialID;feasible\n'
         )
-        for n in [5]:
-            for r in [25.0, 50.0, 100.0, 200.0]:
+        for n in [5, 10, 15, 20, 50, 100, 200, 300]:
+            for r in [25.0, 50.0, 100.0, 200.0]: #, 500.0]:
                 for a in [True, False]:
-                    for i in range(5):
+                    for i in range(4):
                         instance = JobPropertiesInstance.generate_instance(
-                            n, r, adversarial=a, params=params
+                            n, r, adversarial=a, params=params[str(n)]
                         )
 
                         # Solve flow problem
