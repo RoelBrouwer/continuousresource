9621aa7f0c371ff5ac24ec450d9d910596b923f5
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2022-09-01 09:25:23 +0200

    Another parameter setting

--- continuousresource/scripts/runtests_sa.py
+++ continuousresource/scripts/runtests_sa.py
@@ -89,9 +89,9 @@ def main(format, path, output_dir, label, verbose):
     sp_class = SearchSpaceCombined
     slackpenalties = [5, 5]
     sa_params = {
-        'initial_temperature_func': (lambda n: n),
+        'initial_temperature_func': (lambda n: 2 * n),
         'alfa': 0.95,
-        'alfa_period_func': (lambda n: (2 * n - 1) * 4),
+        'alfa_period_func': (lambda n: (2 * n - 1) * 2),
         'cutoff_func': (lambda n: (2 * n - 1) * 8 * 50)
     }
     spp = {
