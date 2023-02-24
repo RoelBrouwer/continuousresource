5762c058ac6e68267c5184583890777af0ebaab1
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2023-02-24 11:47:51 +0100

    Implement jump point MIP Plus

--- continuousresource/mathematicalprogramming/mipmodels.py
+++ continuousresource/mathematicalprogramming/mipmodels.py
@@ -885,6 +885,35 @@ class JumpPointContinuousMIP(EventOrderBasedMIP):
 
 
 class JumpPointContinuousMIPPlus(JumpPointContinuousMIP):
-    def __init__(self, instance, label):
+    """Class implementing a Mixed Integer Linear Programming model for a
+    resource scheduling problem with continuous time and resource.
+    Extends its parent by adding a number of (redundant) constraints on
+    the problem, to strengthen the solver's performance.
 
+    Parameters
+    ----------
+    instance : Dict of ndarray
+        Dictionary containing the instance data.
+    label : str
+        Label or name for the (solved) instance. Be sure to use one that
+        is sufficiently unique, to avoid conflicts or other unexpected
+        behavior.
+    """
+    def __init__(self, instance, label):
         super().__init__(instance, label)
+        for j in range(self._njobs):
+            # A1. Restrict processing time (upper limit) by lower bound
+            if instance['properties'][j, 1] > 0:
+                self._problem.add_constraint(
+                    ct=self._tvar[2 * j + 1] - self._tvar[2 * j]
+                    - (instance['properties'][j, 0] / instance['properties'][j, 1]) <= 0,
+                    ctname=f"Processing_time_upper_limit_job_{j}"
+                )
+
+            # A2. Restrict processing time (lower limit) by upper bound
+            if instance['properties'][j, 2] > 0:
+                self._problem.add_constraint(
+                    ct=self._tvar[2 * j + 1] - self._tvar[2 * j]
+                    - (instance['properties'][j, 0] / instance['properties'][j, 2]) >= 0,
+                    ctname=f"Processing_time_lower_limit_job_{j}"
+                )
