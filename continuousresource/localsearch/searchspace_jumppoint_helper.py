749c1dbd3ae3d35e800d1c26e8e83aa7eaf3cd44
Roel Brouwer <5979700+RoelBrouwer@users.noreply.github.com>
2023-11-20 16:55:04 +0100

    Implement all JumpPointSearchSpaceData functions

--- continuousresource/localsearch/searchspace_jumppoint_helper.py
+++ continuousresource/localsearch/searchspace_jumppoint_helper.py
@@ -1,9 +1,5 @@
-import copy
 import math
 import numpy as np
-import time
-import os
-import os.path
 import warnings
 
 from continuousresource.mathematicalprogramming.eventorder \
@@ -135,6 +131,40 @@ class JumpPointSearchSpaceData():
         """
         return self._resource_availability_table
 
+    def instance_update(self, event_idx, new_idx):
+        """Update the instance to reflect moving an event from
+        `event_idx` to `new_idx`.
+
+        Parameters
+        ----------
+        event_idx : int
+            Original position of the moved event
+        new_idx : int
+            New position of the moved event
+        """
+        self._instance['eventmap'][
+            self._instance['eventlist'][event_idx, 1],
+            self._instance['eventlist'][event_idx, 0]
+        ] = new_idx
+        if new_idx < event_idx:
+            for idx in range(new_idx, event_idx):
+                self._instance['eventmap'][
+                    self._instance['eventlist'][idx, 1],
+                    self._instance['eventlist'][idx, 0]
+                ] += 1
+            self._instance['eventlist'][new_idx:event_idx + 1] = \
+                np.roll(self._instance['eventlist'][new_idx:event_idx + 1],
+                        1, axis=0)
+        else:
+            for idx in range(event_idx + 1, new_idx + 1):
+                self._instance['eventmap'][
+                    self._instance['eventlist'][idx, 1],
+                    self._instance['eventlist'][idx, 0]
+                ] -= 1
+            self._instance['eventlist'][event_idx:new_idx + 1] = \
+                np.roll(self._instance['eventlist'][event_idx:new_idx + 1],
+                        -1, axis=0)
+
     def base_initiate(self):
         """Compute the score of a feasible solution respecting the
         current event order.
@@ -147,7 +177,7 @@ class JumpPointSearchSpaceData():
         """
         self._jumppoint_map = np.zeros(shape=self._njobs, dtype=int)
         self._base_cost = 0
-        
+
         completed = np.zeros(shape=self._njobs, dtype=bool)
 
         for [etype, job] in self._instance['eventlist']:
@@ -281,25 +311,7 @@ class JumpPointSearchSpaceData():
         self._resource_availability_table = np.zeros(shape=(self._nevents, 2),
                                                      dtype=float)
         for i, [etype, job] in enumerate(self._instance['eventlist']):
-            if etype > 1 and i > 0:
-                curr_res = self._instance['jumppoints'][job, etype - 1] * \
-                    self._instance['constants']['resource_availability']
-                self._resource_availability_table[i, 1] = \
-                    self._resource_availability_table[i - 1, 1] + max(
-                        0,
-                        self._resource_availability_table[i - 1, 0] - curr_res
-                    )
-                self._resource_availability_table[i, 0] = \
-                    min(self._resource_availability_table[i - 1, 0], curr_res)
-            elif etype == 1:
-                self._resource_availability_table[i, 0] = \
-                    self._resource_availability_table[i - 1, 0] + \
-                    self._instance['properties'][job, 0]
-                self._resource_availability_table[i, 1] = \
-                    self._resource_availability_table[i - 1, 1]
-            elif i > 0:
-                self._resource_availability_table[i] = \
-                    self._resource_availability_table[i - 1]
+            self._resource_estimation_update(i, job, etype)
 
         return [
             ("resource", self._resource_availability_table[-1, 1],
@@ -354,31 +366,8 @@ class JumpPointSearchSpaceData():
                  self._instance['constants']['slackpenalties'][1])
             ]
 
-        # Determine id of current event
-        orig_id = job * 2 + etype
-
         # Update the mapping for the closest fixed time events
-        new_pred = -1
-        if event_idx > 0:
-            pred_job = self._instance['eventlist'][event_idx - 1, 1]
-            pred_etype = self._instance['eventlist'][event_idx - 1, 0]
-            if pred_etype <= 1:
-                new_pred = \
-                    self._fixed_predecessor_map[pred_job * 2 + pred_etype]
-            else:
-                new_pred = self._nplannable + \
-                    self._kextra * pred_job + pred_etype - 2
-
-        new_succ = -1
-        if event_idx < self._nevents - 1 :
-            succ_job = self._instance['eventlist'][event_idx + 1, 1]
-            succ_etype = self._instance['eventlist'][event_idx + 1, 0]
-            if succ_etype <= 1:
-                new_succ = \
-                    self._fixed_successor_map[succ_job * 2 + succ_etype]
-            else:
-                new_succ = self._nplannable + \
-                    self._kextra * succ_job + succ_etype - 2
+        new_pred, new_succ = self._get_new_neighbors(event_idx, new_idx)
 
         # 1 & 2. Compute the lower/upper bound difference
         old_min_lb, old_min_ub = self._bound_contributions(job)
@@ -390,14 +379,28 @@ class JumpPointSearchSpaceData():
             new_min_lb, new_min_ub = self._bound_contributions(
                 job, pre_compl=new_pred, suc_compl=new_succ
             )
-            
-        
 
         # 3. Recompute the resource estimation
-        # TODO
+        # Update the eventlist AND eventmap
+        self.instance_update(event_idx, new_idx)
+
+        first = min(event_idx, new_idx)
+        last = max(event_idx, new_idx)
+
+        # Store E_shortage from current structure to compute difference
+        prev_short = self._resource_availability_table[last, 1]
+
+        for i, [etype, job] in enumerate(
+            self._instance['eventlist'][first:last + 1, :]
+        ):
+            self._resource_estimation_update(i + first, job, etype)
+
+        diff_short = self._resource_availability_table[last, 1] - prev_short
+        self._resource_availability_table[last + 1:self._nevents, 1] += \
+            diff_short
 
         return [
-            ("resource", 0,
+            ("resource", diff_short,
              self._instance['constants']['slackpenalties'][0]),
             ("upperbound", new_min_ub - old_min_ub,
              self._instance['constants']['slackpenalties'][1]),
@@ -405,26 +408,80 @@ class JumpPointSearchSpaceData():
              self._instance['constants']['slackpenalties'][1])
         ]
 
-    def simple_update_apply(self, success=True):
+    def simple_update_apply(self, event_idx, new_idx, success=True):
         """Apply or revert the changes of the corresponding update.
 
         Parameters
         ----------
+        event_idx : int
+            Original position of the moved event
+        new_idx : int
+            New position of the moved event
         success : boolean
             Whether the update has been accepted or not. If true, the
             changes will be applied, otherwise, they will be reverted.
         """
         if success:
             # Keep changes, set flags.
+            job = self._instance['eventlist'][event_idx, 1]
+            etype = self._instance['eventlist'][event_idx, 0]
+
+            # Update the mapping for the closest fixed time events
+            new_pred, new_succ = self._get_new_neighbors(event_idx, new_idx)
+            self._fixed_predecessor_map[job * 2 + etype] = new_pred
+            self._fixed_successor_map[job * 2 + etype] = new_succ
 
             self._flow_valid = False
             self._lp_valid = False
-            raise NotImplementedError
         else:
             # Revert changes
-            raise NotImplementedError
+            self.instance_update(new_idx, event_idx)
 
-    def _bound_contributions(job, pre_start=None, pre_compl=None,
+            first = min(event_idx, new_idx)
+            last = max(event_idx, new_idx)
+
+            # Store E_shortage from current structure to compute difference
+            prev_short = self._resource_availability_table[last, 1]
+
+            for i, [etype, job] in enumerate(
+                self._instance['eventlist'][first:last + 1, :]
+            ):
+                self._resource_estimation_update(i + first, job, etype)
+
+            diff_short = self._resource_availability_table[last, 1] \
+                - prev_short
+            self._resource_availability_table[last + 1:self._nevents, 1] += \
+                diff_short
+
+    def _get_new_neighbors(self, event_idx, new_idx):
+        mod = 0
+        if new_idx < event_idx:
+            mod = -1
+        new_pred = -1
+        # TODO: event_idx should be new_idx!
+        if new_idx + mod >= 0:
+            pred_job = self._instance['eventlist'][new_idx + mod, 1]
+            pred_etype = self._instance['eventlist'][new_idx + mod, 0]
+            if pred_etype <= 1:
+                new_pred = \
+                    self._fixed_predecessor_map[pred_job * 2 + pred_etype]
+            else:
+                new_pred = self._nplannable + \
+                    self._kextra * pred_job + pred_etype - 2
+
+        new_succ = -1
+        if new_idx + mod < self._nevents - 1:
+            succ_job = self._instance['eventlist'][new_idx + mod + 1, 1]
+            succ_etype = self._instance['eventlist'][new_idx + mod + 1, 0]
+            if succ_etype <= 1:
+                new_succ = \
+                    self._fixed_successor_map[succ_job * 2 + succ_etype]
+            else:
+                new_succ = self._nplannable + \
+                    self._kextra * succ_job + succ_etype - 2
+        return new_pred, new_succ
+
+    def _bound_contributions(self, job, pre_start=None, pre_compl=None,
                              suc_start=None, suc_compl=None):
         """Compute contributions of a job to lower and upper bound
         penalty term estimations. Uses the fixed maps, unless explicitly
@@ -510,6 +567,39 @@ class JumpPointSearchSpaceData():
         )
 
         return min_lb, min_ub
+
+    def _resource_estimation_update(self, i, job, etype):
+        """Performs a single step of the estimation algorithm for the
+        resource penalty.
+
+        Parameters
+        ----------
+        i : int
+            Position (index) in the eventlist
+        job : int
+            Job ID associated with the event at this position
+        etype : int
+            Event type of the event at this position
+        """
+        if etype > 1 and i > 0:
+            curr_res = self._instance['jumppoints'][job, etype - 1] * \
+                self._instance['constants']['resource_availability']
+            self._resource_availability_table[i, 1] = \
+                self._resource_availability_table[i - 1, 1] + max(
+                    0,
+                    self._resource_availability_table[i - 1, 0] - curr_res
+                )
+            self._resource_availability_table[i, 0] = \
+                min(self._resource_availability_table[i - 1, 0], curr_res)
+        elif etype == 1:
+            self._resource_availability_table[i, 0] = \
+                self._resource_availability_table[i - 1, 0] + \
+                self._instance['properties'][job, 0]
+            self._resource_availability_table[i, 1] = \
+                self._resource_availability_table[i - 1, 1]
+        elif i > 0:
+            self._resource_availability_table[i] = \
+                self._resource_availability_table[i - 1]
 
     def flow_initiate(self):
         """Compute the flow approximation of the penalty term for the
@@ -607,7 +697,7 @@ class JumpPointSearchSpaceData():
         if etype > 1:
             orig_id += self._nplannable - 2 + job * (self._kextra - 2)
 
-        curr = orig_idx
+        curr = event_idx
         inv = 0
         # Invert movement direction
         if event_idx > new_idx:
